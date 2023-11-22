e5281435740ec6bacdf9964cb66dff5d437520f3
mar4587 <marshetnigatu@yahoo.com>
2023-11-22 04:30:01 -0800

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This function defines a class Square"""
+
+
+class Square:
+    """This represents a square
+    Attributes:
+        __size (int): the side of the square
+    """
+    def __init__(self, size=0):
+        """This initializes the square
+        Args:
+            size (int): side of the square
+        Returns:
+            None
+        """
+        self.size = size
+
+    def area(self):
+        """This calculates the square's area
+        Returns:
+            area of the square
+        """
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """getter of __size
+        Returns:
+            the size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of __size
+        Args:
+            value (int): side of square
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+
+            else:
+                self.__size = value

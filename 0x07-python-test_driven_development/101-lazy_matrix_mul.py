c9b7af092a0b4556275d663d6c851c2d275da4c3
mar4587 <marshetnigatu@yahoo.com>
2023-12-01 05:20:25 -0800

    Task 101

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Module lazy_matrix_mul
-Matrix multiplication using NumPy
 """
+multiplies two matrices
+"""
+
 import numpy
+"""
+This module contains useful linear algebra operations
+"""
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Multiplies m_a and m_b Using
-    matmul, and returns the result.
-    """
+    """multiplies two matrices"""
     return numpy.matmul(m_a, m_b)
